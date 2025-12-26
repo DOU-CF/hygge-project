5c7fcbcff00fc3e2b67f3bd549464393fd3170b5
DOU-CF <chiafei5757@gmail.com>
2025-12-26 13:39:51 +0800

    feat: Day 4 新增 Dock 點擊事件

--- js/app.js
+++ js/app.js
@@ -3,3 +3,20 @@ console.log("Hygge 專案啟動成功！");
 // 測試：顯示當前時間
 const now = new Date();
 console.log("現在時間：", now.toLocaleString("zh-TW"));
+
+
+// Day 4: Dock 點擊互動
+// 選取所有 Dock 工具
+const dockItems = document.SelectorAll('.dock-item');
+
+
+// 為每個工具加上點擊事件
+dockItems.forEach(item => {
+  item.addEventListener ('click',function(){
+    // 取得工具的類型
+    const widgetType = this.dataset.widget;
+    //先用 alert 測試
+    alert('你點擊了：' + widgetType);
+    });
+
+console.log('Dock 點擊事件已載入！'）；
